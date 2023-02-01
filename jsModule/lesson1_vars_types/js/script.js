b9bf0d101bfdfc60bb2a4d893b52efa20101e129
webader <alexey.derenko@gmail.com>
2023-02-01 12:37:18 +0200

    complete lesson1_vars_types module js

---
+++ jsModule/lesson1_vars_types/js/script.js
@@ -0,0 +1,38 @@
+
+let firstGreeting = 'hello',
+    secondGreeting = "owu",
+    thirdGreeting = 'com',
+    fourthGreeting = "ua",
+    one = 1,
+    ten = 10,
+    minusThousand = -999,
+    hundred = 123,
+    piFloat = 3.14,
+    nextFloat = 2.7,
+    trueing = true,
+    falseing = false;
+
+let firstName = 'Oleksii',
+    lastName = 'Derenko',
+    middleName = 'Volodimirovich'
+
+let fullName = `${firstName} ${middleName} ${lastName}`
+
+console.log(fullName);
+
+let a = 100
+let b = '100'
+let c = true;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+let namePrompt = prompt('Yure name please');
+let middlePrompt = prompt('Yure middle name please');
+let lastPrompt = prompt('Yure last mame please');
+
+
+console.log(`Hello ${namePrompt} ${middlePrompt} ${lastPrompt} `);
+
+
